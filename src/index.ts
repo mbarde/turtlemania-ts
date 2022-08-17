a71b0494f3c05d58cbfa51003bcc2bb25be94e4d
Matthias Barde <matthiasbarde@gmx.de>
2022-08-17 21:25:33 +0200

    fix responsive wid and height

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Config } from './config';
 import { Game } from './game';
 
+const CANVAS_MAX_WIDTH = 800;
+const CANVAS_MAX_HEIGHT = 500;
+
 // read parameters from script-tag
 function getAttributeSafe(el: HTMLOrSVGScriptElement, attrName: string, defaultValue: string) {
 	let value = el.getAttribute(attrName) as string;
@@ -20,13 +23,19 @@ if (clr === null) {
 	turtleColor = clr;
 }
 
-// get or create canvas
+// get canvas and set dimensions
 let canvas: HTMLCanvasElement;
 let canvasId: string;
 canvasId = 'turtlemania-canvas';
 canvas = document.getElementById(canvasId) as HTMLCanvasElement;
-canvas.width = window.screen.width / 2;
-canvas.height = window.screen.height / 2;
+canvas.width = window.screen.width > CANVAS_MAX_WIDTH ? CANVAS_MAX_WIDTH : window.screen.width;
+canvas.height = window.screen.height > CANVAS_MAX_HEIGHT ? CANVAS_MAX_HEIGHT : window.screen.height;
+
+if (canvas.width === window.screen.width) {
+	// remove border left and right to avoid horizontal scrollbar
+	canvas.style.borderLeft = 'none';
+	canvas.style.borderRight = 'none';
+}
 
 let gameConfig = new Config(coinColor, fontColor, turtleColor);
 let game = new Game(canvas, gameConfig)
